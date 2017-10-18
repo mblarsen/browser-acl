745cd04e9963b3a016148dcf621381ac5390b743
Michael Bøcker-Larsen <mblarsen@gmail.com>
2017-10-19 00:45:26 +0800

    test: More tests

--- test/Acl.test.js
+++ test/Acl.test.js
@@ -2,7 +2,11 @@ import Acl from '../'
 
 class User {}
 class Apple {}
-class Job {}
+class Job {
+  constructor(data) {
+    Object.assign(this, data || {})
+  }
+}
 
 test('Acl mixin', () => {
   const acl = new Acl()
@@ -39,24 +43,90 @@ test('Can eat apples (Class)', () => {
   acl.mixin(User)
   acl.rule(['eat'], Apple)
   const user = new User()
-  expect(user.can('eat', 'Apple')).toBe(true)
   expect(user.can('eat', Apple)).toBe(true)
+})
+
+test('Can eat apples (Object)', () => {
+  const acl = new Acl()
+  acl.mixin(User)
+  acl.rule(['eat'], Apple)
+  const user = new User()
   expect(user.can('eat', new Apple())).toBe(true)
 })
 
-test('Scribe-Assist', () => {
+test('Can create jobs complex', () => {
   const acl = new Acl()
+  acl.mixin(User)
   const owner = new User()
   const coworker = new User()
-  const ScribeAssist = {
-    active_organization: {
-      users: [{user: owner, role: 'owner'}, {user: coworker, role: 'coworker'}]
-    }
+
+  const company = {
+    users: [
+      {user: owner, role: 'owner'},
+      {user: coworker, role: 'coworker'}
+    ]
   }
-  acl.mixin(User)
-  acl.rule(['create'], Job, (user, subject) => {
-    return ScribeAssist.active_organization.users.find(rel => rel.user === user && rel.role === 'owner')
+
+  const job = new Job({
+    users: [
+      {user: owner, role: 'owner'},
+      {user: coworker, role: 'coworker'}
+    ]
   })
+
+  acl.rule(['create'], Job, (user) => {
+    return company.users.find(rel => rel.user === user && rel.role === 'owner')
+  })
+
+  acl.rule(['view'], Job, (user, subject) => {
+    return subject.users.find(rel => rel.user === user) ||
+      data.company.users.find(rel => rel.user === user && rel.role === 'owner')
+  })
+
   expect(owner.can('create', Job)).toBe(true)
   expect(coworker.can('create', Job)).toBe(false)
+
+  expect(owner.can('view', job)).toBe(true)
+  expect(coworker.can('view', job)).toBe(true)
+})
+
+test('Policy', () => {
+  const acl = new Acl()
+  acl.mixin(User)
+  const owner = new User()
+  const coworker = new User()
+
+  const data = {
+    company: {
+      users: [
+        {user: owner, role: 'owner'},
+        {user: coworker, role: 'coworker'}
+      ]
+    }
+  }
+
+  const job = new Job({
+    users: [
+      {user: owner, role: 'owner'},
+      {user: coworker, role: 'coworker'}
+    ]
+  })
+
+  const policy = {
+    create: function (user, subject) {
+      return data.company.users.find(rel => rel.user === user && rel.role === 'owner')
+    },
+    view: function (user, subject) {
+      return subject.users.find(rel => rel.user === user) ||
+        data.company.users.find(rel => rel.user === user && rel.role === 'owner')
+    },
+  }
+
+  acl.policy(policy, Job)
+
+  expect(owner.can('create', Job)).toBe(true)
+  expect(coworker.can('create', Job)).toBe(false)
+
+  expect(owner.can('view', job)).toBe(true)
+  expect(coworker.can('view', job)).toBe(true)
 })
